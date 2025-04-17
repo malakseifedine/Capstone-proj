4e39b44d9ae8a4265b74d58240f5b0aa4d6355b3
malak <malakseifeddine966@gmail.com>
2025-04-17 16:48:47 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
+import authRoutes from '../routes/authRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -10,5 +10,5 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5173;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
